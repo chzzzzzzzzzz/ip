9ece05b06cf714e377e6637c282654a8c21e2463
chzzzzzzzzzz <e1525179@u.nus.edu>
2026-08-21 01:00:10 +0800

    Level 0: Edited AGENTS.md and rename chatbot to bot

---
+++ src/main/java/Bot.java
@@ -0,0 +1,12 @@
+public class Bot {
+    public static void main(String[] args) {
+        String banner = """
+                         ____        _
+                        | __ )  ___ | |_
+                        |  _ \\ / _ \\| __|
+                        | |_) | (_) | |_
+                        |____/ \\___/ \\__|
+                        """;
+        System.out.println(banner);
+    }
+}
