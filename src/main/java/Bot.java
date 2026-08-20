c86078e88ca5cc66c1868d2ca0c2689721afc167
chzzzzzzzzzz <e1525179@u.nus.edu>
2026-08-21 01:06:06 +0800

    Level 0: Added greetings and exit lines for chatbot

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,5 +1,6 @@
 public class Bot {
     public static void main(String[] args) {
+        String line = "_".repeat(60);
         String banner = """
                          ____        _
                         | __ )  ___ | |_
@@ -7,6 +8,12 @@ public class Bot {
                         | |_) | (_) | |_
                         |____/ \\___/ \\__|
                         """;
-        System.out.println(banner);
+        System.out.print(banner);
+        System.out.println("Yo! I'm Bot.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+
     }
 }
